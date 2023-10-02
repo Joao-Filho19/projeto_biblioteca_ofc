b1e63323061e43f01b4e10854349b85d52b27645
Joao Mateus <jmbp.ecp19@uea.edu.br>
2023-10-02 09:14:43 -0400

    Atualização ConsultaLivro

--- models/ConsultaLivroModel.php
+++ models/ConsultaLivroModel.php
@@ -3,12 +3,20 @@
 	date_default_timezone_set('America/Sao_Paulo');
 	class ConsultaLivroModel extends Model
 	{
+		// Método para listar os livros agrupados pelo título, incluindo o autor
+		public static function listarLivrosAgrupados()
+		{
+			$query = "
+				SELECT letra_titulo, letra_sobrenome, COUNT(*) as quantidade
+				FROM livros
+				GROUP BY letra_titulo, letra_sobrenome
+			";
 
-		//Aqui é o método para puxar os clientes!
-		public static function listarLivros(){
-		$livros = \MySql::connect()->prepare("SELECT * FROM livros");
-		$livros->execute();
-		return $livros->fetchAll();
+			$livros = \MySql::connect()->query($query);
+			return $livros->fetchAll();
 		}
+
 	}
+
+
 ?>
--- views/templates/consultaLivro.php
+++ views/templates/consultaLivro.php
@@ -1,29 +1,23 @@
 <main>
-    <h2>Acervo de Livros</h2>
+    <h2>Livros presentes no acervo</h2>
     <table id="customers" style="border: 1px solid black">
         <tr>
-                <th>Codigo</th>
-                <th>CDD</th>
-                <th>Autor</th>
-                <th>Titulo</th>
-                <th>Exemplar</th>
-                <th>Edição</th>
+            <th>Autor</th>
+            <th>Título</th>
+            <th>Quantidade de Exemplares</th>
         </tr>
         <?php 
-            $livros = \Models\ConsultaLivroModel::listarLivros();
-            foreach ($livros as $value) {
+        $livrosAgrupados = \Models\ConsultaLivroModel::listarLivrosAgrupados();
+        foreach ($livrosAgrupados as $value) {
         ?>
         <tr>
-            <td><?php echo $value['cod_livro']?></td>
-            <td><?php echo $value['cdd']?></td>
             <td><?php echo $value['letra_sobrenome']?></td>
             <td><?php echo $value['letra_titulo']?></td>
-            <td><?php echo $value['exemplar']?></td>
-            <td><?php echo $value['edicao']?></td>
+            <td><?php echo $value['quantidade']?></td>
         </tr>
-
         <?php 
-            }
+        }
         ?>
     </table>
+
 </main>
